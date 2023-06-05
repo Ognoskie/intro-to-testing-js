f3335e87b99829b59acd3f2b7ea920190e177d51
Braden Wittkop <bradenjwittkop@gmail.com>
2023-06-05 11:40:53 -0500

    add the first test and solution for intro-to-testing-a

--- code.js
+++ code.js
@@ -1,4 +1,18 @@
-// helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// // // helloWorld function
+// // function helloWorld() {
+// //     return "Hello, World!";
+// // }
+//
+// // function expression syntax (assigning an anonymous function to a variable)
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+
+//  function helloWorld() {
+//      return "Hello, World!";
+// }
+
+function sayHello() {
+
 }
