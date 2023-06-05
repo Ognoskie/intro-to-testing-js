5507a35018cd77b0c4e32099f5def5b2456d0717
Braden Wittkop <bradenjwittkop@gmail.com>
2023-06-05 12:32:27 -0500

    add to intro-to-testing-a

--- code.js
+++ code.js
@@ -13,6 +13,12 @@
 //      return "Hello, World!";
 // }
 
-function sayHello() {
-    return "";
+function sayHello(input) {
+    if (input === "Alex") {
+            return "Hello, Alex!"
+        }else if (input === "Pat") {
+            return "Hello, Pat!"
+        }else {
+            return "Hello, Jane!"
+        }
 }
